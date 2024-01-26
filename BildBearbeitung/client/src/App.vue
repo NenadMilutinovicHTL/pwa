38d6ba7c9d00c72ed0f6f609abe82092821761a1
NenadMilutinovicHTL <99362213+NenadMilutinovicHTL@users.noreply.github.com>
2024-01-26 21:16:01 +0100

    Add files via upload

---
+++ BildBearbeitung/client/src/App.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { ref } from 'vue';
+
+const leftDrawerOpen = ref(false);
+
+
+const toggleLeftDrawer = () => (leftDrawerOpen.value = !leftDrawerOpen.value);
+</script>
+
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header
+      elevated
+      class="text-white"
+      height-hint="98"
+      style="background-color: #000"
+    >
+      <q-toolbar>
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          @click="toggleLeftDrawer"
+          class="lt-md"
+        />
+        <q-toolbar-title class="raleway">
+          <q-avatar>
+            <img src="/public/Icon.png" />
+          </q-avatar>
+          Bildbearbeitung
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left" class="gt-sm">
+        <q-route-tab to="/" label="Home" />
+        <q-route-tab to="/impressum" label="Impressum" />
+      </q-tabs>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" bordered>
+      <q-tabs vertical>
+        <q-route-tab to="/" label="Home" icon="fa fa-home" />
+        <q-route-tab to="/impressum" label="Impressum" icon="fa fa-info" />
+      </q-tabs>
+    </q-drawer>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style>
+body {
+  background-color: #dbdbdb;
+}
+
+@font-face {
+  font-family: 'Montserrat';
+  src: url('/fonts/Montserrat/Montserrat-Regular.ttf') format('truetype');
+}
+
+@font-face {
+  font-family: 'Raleway';
+  src: url('/fonts/Raleway/Raleway-ExtraBold.ttf');
+}
+
+.raleway {
+  font-family: 'Raleway';
+}
+
+* {
+  font-family: 'Montserrat';
+}
+</style>
---
+++ BildBearbeitung/client/src/views/HomeView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { usePicturesStore } from '../stores/picturesStore';
+
+const store = usePicturesStore();
+store.getPictures();
+
+const cols = [
+  { name: 'isOriginal', field: 'isOriginal', label: 'isOriginal', align: 'left' },
+  { name: 'timestamp', field: 'timestamp', label: 'timestamp', align: 'right' },
+  { name: 'img', field: 'img', label: 'img', align: 'left' },
+  { name: 'image', field: 'image', label: 'image', align: 'left' },
+];
+</script>
+
+<template>
+  <div class="q-mt-md q-ml-sm q-mr-sm">
+    <q-card>
+      <q-table
+        grid
+        title="Pictures"
+        :rows="store.pictures"
+        :columns="cols"
+        row-key="name"
+        :pagination="{ rowsPerPage: 10 }"
+        class="lt-sm"
+      >
+      </q-table>
+    </q-card>
+    <q-card>
+      <q-table
+        title="Pictures"
+        :rows="store.pictures"
+        :columns="cols"
+        row-key="name"
+        :pagination="{ rowsPerPage: 10 }"
+        class="gt-xs"
+      >
+      </q-table>
+    </q-card>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
