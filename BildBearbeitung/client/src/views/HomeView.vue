09842e8fe50b0af9f5abdf8c7b690fe517bf184f
NenadMilutinovicHTL <99362213+NenadMilutinovicHTL@users.noreply.github.com>
2024-02-19 15:26:02 +0100

    changing this view to other view

--- BildBearbeitung/client/src/views/HomeView.vue
+++ BildBearbeitung/client/src/views/HomeView.vue
@@ -1,42 +1,10 @@
 <script setup>
-import { usePicturesStore } from '../stores/picturesStore';
-
-const store = usePicturesStore();
-store.getPictures();
-
-const cols = [
-  { name: 'isOriginal', field: 'isOriginal', label: 'isOriginal', align: 'left' },
-  { name: 'timestamp', field: 'timestamp', label: 'timestamp', align: 'right' },
-  { name: 'img', field: 'img', label: 'img', align: 'left' },
-  { name: 'image', field: 'image', label: 'image', align: 'left' },
-];
+import Camera from './Camera.vue';
 </script>
 
 <template>
-  <div class="q-mt-md q-ml-sm q-mr-sm">
-    <q-card>
-      <q-table
-        grid
-        title="Pictures"
-        :rows="store.pictures"
-        :columns="cols"
-        row-key="name"
-        :pagination="{ rowsPerPage: 10 }"
-        class="lt-sm"
-      >
-      </q-table>
-    </q-card>
-    <q-card>
-      <q-table
-        title="Pictures"
-        :rows="store.pictures"
-        :columns="cols"
-        row-key="name"
-        :pagination="{ rowsPerPage: 10 }"
-        class="gt-xs"
-      >
-      </q-table>
-    </q-card>
+  <div>
+   
   </div>
 </template>
 
